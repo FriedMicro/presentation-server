9c3f8b20a9809e31bd0f027e022c99da1b0356a8
Lucas Smith <lucas@lucasesmith.com>
2021-07-15 00:45:59 +0000

    Update frontend to modern coding standards.

--- frontend/main.js
+++ frontend/main.js
@@ -1,36 +1,43 @@
-pageNumber = 0;
+let pageNumber = 0;
 
-changePage = function(incrementCount){
+const changePage = (incrementCount) => {
   pageNumber += incrementCount;
-  var params = buildParams(pageNumber);
-  sendRequest(params, function(){});
+  const params = buildParams(pageNumber);
+  sendRequest(params);
 }
 
-goToStart = function(){
+const goToStart = () => {
   pageNumber = 0;
-  var params = buildParams(pageNumber);
-  sendRequest(params, function(){});
+  const params = buildParams(pageNumber);
+  sendRequest(params);
 }
 
-buildParams = function(page){
-  var password = document.getElementById("password").value;
-  var params = {
+const buildParams = (page) => {
+  const password = readDomPassword();
+  const params = {
     api_key: password,
     'page_number': page
   }
   return JSON.stringify(params);
 }
 
-sendRequest = function(params, callback){
-  var http = new XMLHttpRequest();
-  var destination = window.location.protocol + "//" + window.location.host;
+const readDomPassword = () => {
+  const password = document.getElementById("password").value;
+  return password;
+}
+
+const sendRequest = (params) => {
+  const http = new XMLHttpRequest();
+  const destination = window.location.protocol + "//" + window.location.host;
   http.open("POST", destination + "/presentation-remote", true);
   http.setRequestHeader("Content-type", "application/json");
-  http.onreadystatechange = function() {
-    if (http.readyState == 4) {
-      var response = JSON.parse(http.responseText);
-      callback();
-    }
-  };
-  http.send(params);
+  return new Promise((resolve, reject) => {
+    http.onreadystatechange = () => {
+      if (http.readyState === 4) {
+        const response = JSON.parse(http.responseText);
+        resolve(response);
+      }
+    };
+    http.send(params);
+  })
 }

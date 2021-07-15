ad6601607f0a36bf025adaf3591787343659970c
Lucas Smith <lucas@lucasesmith.com>
2021-07-15 00:02:32 +0000

    Update libs and coding style to modern JS.

---
+++ config.example.js
@@ -0,0 +1,6 @@
+export default {
+  password: "",
+  apiSocket: 3000,
+  socketPort: 5000,
+  host: 'localhost'
+}
--- main.js
+++ main.js
@@ -1,32 +1,23 @@
-//NODEJS libraries
-var express = require('express');
-var http = require('http');
-var socketIo = require('socket.io');
-var bodyParser = require('body-parser');
-var config = require('config.js');
-
-//Constants
-const APIPORT = 3000;
-const SOCKETPORT = 5000;
-const HOST = 'localhost';
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import config from './config.js';
 
 //Create server and socket connections
-var apiServer = express();
-var socketsServer = http.createServer();
-var sockets = socketIo.listen(socketsServer);
+const apiServer = express();
+const socketsServer = http.createServer();
+const sockets = new Server(socketsServer);
 
 //Setup body parser
-apiServer.use(bodyParser.json());
-
 //REST API
-apiServer.post('/presentation-remote', function(request, response){
-  if(request.body.api_key == ){
-    var page = request.body.page_number;
+apiServer.post('/presentation-remote', (request, response) => {
+  if (request.body.api_key === config.password) {
+    const page = request.body.page_number;
     sockets.emit("presentation-page", page);
   }
-  response.status(200).send({data: 'ok'});
+  response.status(200).send({ data: 'ok' });
 })
 
 //Open ports for API and sockets
-apiServer.listen(APIPORT, HOST);
-socketsServer.listen(SOCKETPORT, HOST);
+apiServer.listen(config.apiSocket, config.host);
+socketsServer.listen(config.socketPort, config.host);
